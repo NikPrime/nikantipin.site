790e628764307b546624736610ad79f5330a08e3
Nikita <nikprime1@gmail.com>
2022-12-26 00:13:15 +0300

    test deploy

--- apps/backend/src/modules/app/app.module.ts
+++ apps/backend/src/modules/app/app.module.ts
@@ -15,7 +15,7 @@ import { MemesModule } from '../memes/memes.module';
             type: 'postgres',
             host: process.env.POSTGRES_HOST || 'localhost',
             port: parseInt(process.env.POSTGRES_PORT) || 5432,
-            username: process.env.POSTGRES_USERNAME || 'postgres',
+            username: process.env.POSTGRES_USER || 'postgres',
             password: process.env.POSTGRES_PASSWORD || 'password',
             database: process.env.POSTGRES_DB || 'nikantipin',
             entities: ['dist/**/**.entity{.ts,.js}'],
