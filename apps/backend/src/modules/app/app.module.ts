d261edd1fd933996daae5d88af68a92de47645ae
Nikita <nikprime1@gmail.com>
2022-12-26 22:41:03 +0300

    test deploy

--- apps/backend/src/modules/app/app.module.ts
+++ apps/backend/src/modules/app/app.module.ts
@@ -8,6 +8,13 @@ import { TokensModule } from '../tokens/tokens.module';
 import { ArticlesModule } from "../articles/articles.module";
 import { MemesModule } from '../memes/memes.module';
 
+// for test
+console.log('POSTGRES_HOST: ', process.env.POSTGRES_HOST)
+console.log('POSTGRES_PORT: ', process.env.POSTGRES_PORT)
+console.log('POSTGRES_DB: ', process.env.POSTGRES_DB)
+console.log('POSTGRES_USER: ', process.env.POSTGRES_USER)
+console.log('POSTGRES_PASSWORD: ', process.env.POSTGRES_PASSWORD)
+
 @Module({
     imports: [
         ConfigModule.forRoot(),
