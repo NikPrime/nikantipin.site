08434aa8ed08c531ee32d214f0b487bac367a01a
Nikita <nikprime1@gmail.com>
2023-01-22 21:25:38 +0300

    test deploy

--- apps/backend/ormconfig.ts
+++ apps/backend/ormconfig.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm';
 import { join } from 'path';
 
-const {POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD} = process.env;
+const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
 
 // for test
 console.log('POSTGRES_HOST: ', POSTGRES_HOST)
@@ -12,11 +12,11 @@ console.log('POSTGRES_PASSWORD: ', POSTGRES_PASSWORD)
 
 export const connectionSource = new DataSource({
         type: 'postgres',
-        host: POSTGRES_HOST || 'localhost',
-        port: parseInt(POSTGRES_PORT) || 5432,
-        username: POSTGRES_USER || 'postgres',
-        password: POSTGRES_PASSWORD || 'password',
-        database: POSTGRES_DB || 'nikantipin',
+        host: process.env.POSTGRES_HOST || 'localhost',
+        port: parseInt(process.env.POSTGRES_PORT) || 5432,
+        username: process.env.POSTGRES_USER || 'postgres',
+        password: process.env.POSTGRES_PASSWORD || 'password',
+        database: process.env.POSTGRES_DB || 'postgres',
         migrations: ['src/migrations/*.ts'],
         entities: [join(__dirname, 'src', 'modules', '**', '*.entity{.ts,.js}'), join(__dirname, 'dist', 'modules', '**', '*.entity{.ts,.js}')],
     },
